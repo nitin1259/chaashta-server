2010e6b759f4f78931df22c1907dd616fc37cbdb
Nitin SINGH <nitinkumar.singh@f5.com>
2019-03-19 23:20:32 +0530

    changing port

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const server = http.createServer((req, res) => {
     
 });
 
-server.listen(1337, '127.0.0.1', () => {
+server.listen(3000, '127.0.0.1', () => {
     console.log('Listening for requests now');
 });
 
