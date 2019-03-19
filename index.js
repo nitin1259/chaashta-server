e54bb88e617f1ea3d350c4064a90a84c69e71424
Nitin SINGH <nitinkumar.singh@f5.com>
2019-03-19 23:43:24 +0530

    changing the port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const url = require('url');
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
+const port = process.env.PORT || 1337;
+
 const server = http.createServer((req, res) => {
     
     const pathName = url.parse(req.url, true).pathname;
@@ -56,8 +58,8 @@ const server = http.createServer((req, res) => {
     
 });
 
-server.listen(process.env.PORT || 1337, '127.0.0.1', () => {
-    console.log('Listening for requests now');
+server.listen(port, '127.0.0.1', () => {
+    console.log('Listening for requests now on port ', port);
 });
 
 function replaceTemplate(originalHtml, laptop) {
